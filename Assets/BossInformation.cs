21eb931b9e1cf97b55eeb549ef6413ddd401bf8f
galaxymacos <galaxymaxx@outlook.com>
2018-11-20 03:09:29 -0500

    +Add boss and build
    Still need to upload boss fight gameplay
    Still need to add configuration window image and icon

---
+++ Assets/BossInformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossInformation : MonoBehaviour {
+	private int weaknessNumber = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void BreakWeakness() {
+		weaknessNumber -= 1;
+		if (weaknessNumber == 0) {
+			// Game over
+		}
+	}
+}
--- Assets/Scripts/Enemy Scripts/EnemyTarget.cs
+++ Assets/Scripts/Enemy Scripts/EnemyTarget.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,21 +8,40 @@ public class EnemyTarget : MonoBehaviour {
     [SerializeField] private GameObject dustExplosionEffect;
 
     [SerializeField] private float hp = 200f;
-    
+
+    private Material mat;
+
+    void Start() {
+        mat = GetComponent<Renderer>().material;
+    }
     // Update is called once per frame
     void Update() {
+        
         if (hp <= 0) {
             Die();
         }
     }
 
     public void TakeDamage(float damage) {
+        StartCoroutine(TurnRedAnimation());
         hp -= damage;
         if (hp <= 0)
             Die();
     }
 
+    IEnumerator TurnRedAnimation() {
+        mat.color = Color.red;
+        mat.SetColor("_EmissionColor",Color.red);
+        yield return new WaitForSeconds(0.1f);
+        mat.color = Color.white;
+        mat.SetColor("_EmissionColor",Color.yellow);
+
+    }
+
     public void Die() {
+        if (gameObject.CompareTag("Weakness")) {
+            GetComponent<WeaknessBreak>().BreakWeakness();
+        }
         Instantiate(dustExplosionEffect, transform.position, Quaternion.identity);
         Instantiate(flameEffect, transform.position+new Vector3(0,-1,0), Quaternion.identity);    // To put the fire on the ground
         Destroy(gameObject);
